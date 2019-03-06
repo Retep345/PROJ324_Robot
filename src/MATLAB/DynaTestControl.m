a54ece617cfe11941b1d7600b39660c9d9de2c32
Peter A <32262003+Retep345@users.noreply.github.com>
2019-03-06 12:29:33 +0000

    matlab control
    have the current proto-design frame working, able to move and read under one command with sub commands.

--- src/MATLAB/DynaTestControl.m
+++ src/MATLAB/DynaTestControl.m
@@ -7,21 +7,31 @@ rosinit
 
 rostopic list
 
+
+set = 1;
+get = 0;
+
+%proto model 11 14-15 L, 7 4-5 R
 JointPublisher = [
+%     rospublisher('jointXX_controller/command', 'std_msgs/Float64');
     rospublisher('joint11_controller/command', 'std_msgs/Float64');
-    rospublisher('joint12_controller/command', 'std_msgs/Float64');
-    rospublisher('joint13_controller/command', 'std_msgs/Float64');
     rospublisher('joint14_controller/command', 'std_msgs/Float64');
     rospublisher('joint15_controller/command', 'std_msgs/Float64');
+    rospublisher('joint07_controller/command', 'std_msgs/Float64');
+    rospublisher('joint04_controller/command', 'std_msgs/Float64');
+    rospublisher('joint05_controller/command', 'std_msgs/Float64');
+
 ];
 
 
 JointSubscriber = [
+%     rossubscriber('jointXX_controller/state');
     rossubscriber('joint11_controller/state');
-    rossubscriber('joint12_controller/state');
-    rossubscriber('joint13_controller/state');
     rossubscriber('joint14_controller/state');
     rossubscriber('joint15_controller/state');
+    rossubscriber('joint07_controller/state');
+    rossubscriber('joint04_controller/state');
+    rossubscriber('joint05_controller/state');
 ];
 
 pause(2);
@@ -29,17 +39,20 @@ pause(2);
 
 
 
-DynaAngle(1, JointPublisher(1), 1);
+DynaAngle(set, JointPublisher(1), 1.5);
     
-pause(2);
+pause(2.5);
 
-DynaAngle(1, JointPublisher(1), 0);
-
-pause(2);
-
-angles = DynaAngle(0, JointSubscriber(1), 0);
+angles = DynaAngle(get, JointSubscriber(1), 0);
 angles
+pause(1)
 
+DynaAngle(set, JointPublisher(1), 0);
+
+pause(2);
+
+angles = DynaAngle(get, JointSubscriber(1), 0);
+angles
 pause(1)
 
 
